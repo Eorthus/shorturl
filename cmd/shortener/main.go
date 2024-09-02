b2b3d7ba3b38d4aa630989e043a7f46c67224887
eorthus <oakenshielddis@gmail.com>
2024-09-02 20:48:11 +0300

    feat: create web-server

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,77 @@
 package main
 
-func main() {}
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+)
+
+var (
+	urlMap = make(map[string]string)
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, handleRequest)
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		handlePost(w, r)
+	case http.MethodGet:
+		handleGet(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusBadRequest)
+	}
+}
+
+func handlePost(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Content-Type") != "text/plain" {
+		http.Error(w, "Invalid Content-Type", http.StatusBadRequest)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusBadRequest)
+		return
+	}
+
+	longURL := string(body)
+	if !strings.HasPrefix(longURL, "http://") && !strings.HasPrefix(longURL, "https://") {
+		http.Error(w, "Invalid URL format", http.StatusBadRequest)
+		return
+	}
+
+	shortID := generateShortID()
+	urlMap[shortID] = longURL
+
+	shortURL := "http://localhost:8080/" + shortID
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte(shortURL))
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request) {
+	shortID := strings.TrimPrefix(r.URL.Path, "/")
+	longURL, exists := urlMap[shortID]
+
+	if !exists {
+		http.Error(w, "Short URL not found", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Location", longURL)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func generateShortID() string {
+	b := make([]byte, 6)
+	rand.Read(b)
+	return base64.URLEncoding.EncodeToString(b)[:8]
+}
